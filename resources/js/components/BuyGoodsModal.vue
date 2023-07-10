4177b8ebc06ff96fd817f555e0e46887cf6014aa
Cadregich2 <theloyworkmail@gmail.com>
2023-07-10 14:42:15 +0300

    Fixed the extra zeros in front of the number

--- resources/js/components/BuyGoodsModal.vue
+++ resources/js/components/BuyGoodsModal.vue
@@ -9,11 +9,14 @@
                 <form id="buy-form">
                     <input id="goodsId" type="hidden" name="item-id">
                     <label for="buy-item-range"></label>
-                    <input id="buy-item-range" type="text" value="1" name="items-count">
-                    <input class="form-range" id="itemRange" type="range" value="1" min="1" max="100" step="1">
+                    <input :value="productCount" id="buy-item-range" type="text" name="items-count"
+                           maxlength="3" @input="validateProductCount" inputmode="numeric">
+                    <input v-model="productCount" class="form-range" id="itemRange" type="range" min="1" max="100"
+                           step="1">
                 </form>
                 <label class="buy-label" for="itemRange">Выберите количество</label>
-                <p class="modal-cost"></p>
+                <p class="modal-cost"><i class="cost">{{ this.productCount * productData.price }}</i>
+                    <i class="fa-solid fa-coins modal-coins"></i> за {{ this.productCount }} шт.</p>
             </template>
 
             <template v-slot:modalFooter>
@@ -48,9 +51,35 @@ export default {
     data() {
         return {
             openThanksModal: false,
+            productCount: 1
         }
     },
     methods: {
+        validateProductCount(event) {
+            let value = event.target.value.replace(/^0+/, '');
+            if (value) {
+                if (!isNaN(value)) {
+                    if (value < 1) {
+                        this.productCount = 1;
+                        event.target.value = 1;
+                        value = 1;
+                    }
+                    if (value > 999) {
+                        this.productCount = 999;
+                        event.target.value = 999;
+                        value = 999;
+                    }
+                    this.productCount = value;
+                } else {
+                    if (value) {
+                        event.target.value = 1;
+                    }
+                }
+            } else {
+                this.productCount = 1;
+            }
+        },
+
         openModal(modalId) {
             this.$store.dispatch('openModal', modalId);
         },
@@ -59,6 +88,13 @@ export default {
             this.$store.dispatch('closeModal');
         }
     },
+    watch: {
+        productData: {
+            handler() {
+                this.productCount = 1;
+            },
+        }
+    }
 }
 </script>
 
