3fe4114ec3f9be96f32b1da41a9040545da59463
Cadregich <thekeykeyloy@gmail.com>
2023-12-12 12:57:33 +0200

    Privileges system v1.0. Work with kits

---
+++ database/migrations/2023_11_02_002227_create_kits_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kits', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('server_id');
+            $table->string('command');
+            $table->json('data');
+            $table->timestamps();
+
+            $table->foreign('server_id')->references('id')->on('servers')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kits');
+    }
+};
